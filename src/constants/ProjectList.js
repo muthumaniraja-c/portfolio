a3d9635b627d48603425764c9c10dc57a673886e
muthumaniraja-c <muthumaniraja.c@gmail.com>
2025-12-11 12:00:13 +0530

    update

--- src/constants/ProjectList.js
+++ src/constants/ProjectList.js
@@ -45,37 +45,5 @@ export const ProjectList = [
        
         demo: "https://portfolio-react-sepia-pi-61.vercel.app/",
     },
-    {
-        id: 4,
-        name: "Discord UI",
-        description:
-            "Discord UI Clone is a web application meticulously crafted with React and Tailwind CSS to mirror the essence and functionality of the original Discord UI. It encompasses a faithful reproduction of Discord's home page, Nitro page, and Safety page. The application boasts full responsiveness and furthermore, every link seamlessly connects to the official Discord site.",
-        img: discord,
-       
-        tech: ["React.js", "Tailwind CSS", "Material UI"],
-        source: "https://github.com/aashish-dhiman/discord-clone",
-        demo: "https://discord-aashish.vercel.app/",
-    },
-    {
-        id: 5,
-        name: "Whisper Wave",
-        description:
-            "The Whisper Wave built with Node.js, Express.js, and Passport.js, provides a user-friendly platform for secure registration, secret sharing, and anonymous content viewing. It incorporates Google OAuth 2.0 for seamless login. With a focus on ease of use, it showcases modern web development in a straightforward manner, offering a safe and engaging online experience.",
-        img: authorisation,
-       
-        tech: ["Node.js", "Express.js", "EJS", "MongoDB", "Passport.js"],
-        source: "https://github.com/aashish-dhiman/Authorisation",
-        demo: null,
-    },
-    {
-        id: 6,
-        name: "Keeper - Keep Your Notes",
-        description:
-            "Inspired by Google Keep, Keeper is a note-taking website developed with React and adorned with Tailwind CSS and Material UI. It offers a user-friendly experience, allowing you to effortlessly create and manage notes. The responsive design ensures a seamless experience across all devices, making note-taking a breeze.",
-        img: keeper,
-      
-        tech: ["React.js", "Tailwind CSS", "Material UI"],
-        source: "https://github.com/aashish-dhiman/Keeper-keep-your-notes",
-        demo: "https://keeper-aashish.netlify.app/",
-    },
+ 
 ];
